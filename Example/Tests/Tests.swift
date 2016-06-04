41b775d22a8fbe6fd1c413802d2c855b7666ed9d
John Leszczynski <jrleszczynski@gmail.com>
2016-06-04 12:01:03 -0400

    add basic test

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,27 +3,44 @@ import XCTest
 import SwiftSort
 
 class Tests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testExample() {
+    // This is an example of a functional test case.
+    XCTAssert(true, "Pass")
+  }
+  
+  func testInsertionSort() {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let test = [5, 7, 2, -5, 3, 4, 21, -1, 0, 3]
+    
+    let insertionSorted = test.insertionSort(intIsLessThan)
+    
+    for (i, val) in test.sort(intIsLessThan).enumerate() {
+      XCTAssert(insertionSorted[i] == val)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+  }
+  
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measureBlock() {
+      // Put the code you want to measure the time of here.
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
+  private func intIsLessThan(a: Int, b: Int) -> Bool {
+    return a < b
+  }
+  
 }
